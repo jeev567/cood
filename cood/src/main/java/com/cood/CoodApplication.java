06ffb3023ed754e4bcb8db087a4143ae739ef905
Jeevan Abraham <=jeev567@gmail.com>
2024-01-23 06:32:27 -0700

    first commit

---
+++ cood/src/main/java/com/cood/CoodApplication.java
@@ -0,0 +1,13 @@
+package com.cood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoodApplication.class, args);
+	}
+
+}
